f2e0b56a0c1307bce91e7f62bfd62568a6e92143
awortham <awortham@newlifechurch.tv>
2015-01-03 16:37:06 -0700

    two tests passing

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,4 +1,20 @@
+require './lib/events'
+
 module HabitRabbitClient
   describe 'events' do
+    before do
+      @client = HabitRabbitClient::Event.new('http://localhost:3000')
+    end
+
+    it 'finds an event by id number' do
+      event = @client.find_event(1)
+      expect(event.name).to eq()
+    end
+
+    it 'finds all events' do
+      events = @client.all_events
+      expect(events.first['completed']).to eq(true)
+      expect(events.first['habit_id']).to eq(1)
+    end
   end
 end
--- spec/habit_spec.rb
+++ spec/habit_spec.rb
@@ -1,4 +1,18 @@
+require './lib/habits'
+
 module HabitRabbitClient
   describe 'habits' do
+    before do
+      @client = HabitRabbitClient::Habit.new('http://localhost:3000')
+    end
+    it 'finds a habit by id number' do
+      habit = @client.find_habit(1)
+      expect(habit.name).to eq()
+    end
+
+    it 'finds all habits' do
+      habits = @client.all_habits
+      expect(habits.first['name']).to eq('first exercise')
+    end
   end
 end
